befd60c81d224b1bf559ac6560578f683c500310
Yan17Ig <95976226+Yan17Ig@users.noreply.github.com>
2021-12-30 13:43:00 +0200

    Add files via upload

---
+++ Python_scripts/trg.py
@@ -0,0 +1,11 @@
+import random
+def number():
+    j = (a + b + c + k + l) // x
+    return j
+a = random.randrange(1, 1000)
+b = random.randrange(1, 1000)
+c = random.randrange(1, 1000)
+k = random.randrange(1, 1000)
+l = random.randrange(1, 1000)
+x = 5
+print(number())
